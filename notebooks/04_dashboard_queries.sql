781476f86f7793e2775653bec69ba61e9a5d46c7
ElipechukIgor <elipechukneves@gmail.com>
2026-06-12 20:02:29 -0300

    Update 04_dashboard_queries.sql

--- notebooks/04_dashboard_queries.sql
+++ notebooks/04_dashboard_queries.sql
@@ -1,4 +1,21 @@
--- Dashboard Queries
+-- Total ATM Events
 
-SELECT *
-FROM atm_catalog.gold.atm_health_summary;
+SELECT COUNT(*) AS total_events
+FROM atm_silver;
+
+-- Failure Events
+
+SELECT SUM(failure_events) AS failure_events
+FROM atm_silver;
+
+-- High Temperature Alerts
+
+SELECT COUNT(*) AS high_temperature_alerts
+FROM atm_silver
+WHERE temperature > 80;
+
+-- Low Cash Alerts
+
+SELECT COUNT(*) AS low_cash_alerts
+FROM atm_silver
+WHERE cash_level < 20;
