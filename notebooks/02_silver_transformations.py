d2dc0bb3311ac436fc0d8a9579ce9da08cbdada3
ElipechukIgor <elipechukneves@gmail.com>
2026-06-12 20:01:50 -0300

    Refactor silver transformations with data cleansing
    Updated docstring format and added data cleansing logic.

--- notebooks/02_silver_transformations.py
+++ notebooks/02_silver_transformations.py
@@ -1,5 +1,21 @@
-"""
-Silver Layer
+# Silver Layer
+# Data Cleansing and Enrichment
 
-Data cleansing and enrichment.
-"""
+from pyspark.sql.functions import *
+
+bronze_df = spark.table("atm_bronze")
+
+silver_df = (
+    bronze_df
+    .filter(col("atm_id").isNotNull())
+    .withColumn(
+        "temperature_status",
+        when(col("temperature") > 80, "HIGH")
+        .otherwise("NORMAL")
+    )
+)
+
+silver_df.write \
+    .format("delta") \
+    .mode("overwrite") \
+    .saveAsTable("atm_silver")
